5d8bf23324dc112224b27b46fd90100d59d0a3c1
maria <ma.usachova@gmail.com>
2022-06-08 20:06:09 +0300

    postgresql + article model

--- news_site/mysite/models.py
+++ news_site/mysite/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
-# Create your models here.
+class Article(models.Model):
+    # topic = models.CharField()
+    title = models.CharField(max_length=100)
+    text = models.TextField()
+    image = models.ImageField(blank=True)
+
+    def __str__(self):
+        return self.title
--- news_site/news_site/settings.py
+++ news_site/news_site/settings.py
@@ -56,7 +56,7 @@ ROOT_URLCONF = 'news_site.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, 'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -75,10 +75,21 @@ WSGI_APPLICATION = 'news_site.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.0/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'db_news',
+        'USER': 'admin',
+        'PASSWORD': 'admin',
+        # 'HOST': '127.0.0.1',
+        # 'PORT': '5432',
     }
 }
 
