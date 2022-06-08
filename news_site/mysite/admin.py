09e7e7ee1506748e8c19632157e472790a5e6ca1
maria <ma.usachova@gmail.com>
2022-06-08 22:36:32 +0300

    topic model

--- news_site/mysite/admin.py
+++ news_site/mysite/admin.py
@@ -1,5 +1,10 @@
 from django.contrib import admin
-from .models import Article
+from .models import Topic, Article
+
+class TopicAdmin(admin.ModelAdmin):
+    pass
+
+admin.site.register(Topic, TopicAdmin)
 
 class ArticleAdmin(admin.ModelAdmin):
     pass
--- news_site/mysite/models.py
+++ news_site/mysite/models.py
@@ -1,8 +1,14 @@
 from django.db import models
 
+class Topic(models.Model):
+    topic = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.topic
+
 class Article(models.Model):
     # topic = models.CharField()
-    title = models.CharField(max_length=100)
+    title = models.CharField(max_length=300)
     text = models.TextField()
     image = models.ImageField(blank=True)
 
