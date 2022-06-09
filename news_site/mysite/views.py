a77b6ae35fb9ff56685fa377893e1f83f623ecfe
maria <ma.usachova@gmail.com>
2022-06-09 15:47:16 +0300

    topic view

--- news_site/mysite/views.py
+++ news_site/mysite/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
 from django.views import View
+from .models import Topic, Article
 
 class MainView(View):
     def get(self, request, *args, **kwargs):
-        return render(request, 'mysite/index.html')
+        topics = Topic.objects.all()
+        return render(request, 'mysite/index.html', context={'topics': topics})
