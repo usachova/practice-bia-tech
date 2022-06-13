b61d18f0fb4bbae02baa292fd2f1b72b9db950db
maria <ma.usachova@gmail.com>
2022-06-13 11:09:54 +0300

    страница редактирования тем

--- news_site/news_site/settings.py
+++ news_site/news_site/settings.py
@@ -12,16 +12,16 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 
 import os
 from pathlib import Path
+from decouple import config
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
-
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-sus2eneyioqg7z1a+zxwoxt0n$psy5*c=x7--0!ae7gp$^cr$f'
+SECRET_KEY = config("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
