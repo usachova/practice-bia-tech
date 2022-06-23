0ccee7dec738f0d277e05268d0a9ebf8b1260eea
maria <ma.usachova@gmail.com>
2022-06-23 22:16:38 +0300

    topic tests

--- news_site/mysite/tests/test_views.py
+++ news_site/mysite/tests/test_views.py
@@ -26,52 +26,71 @@ class TopicViewTest(TestCase):
         self.assertEqual(test_response.status_code, 200)
         self.assertTrue('topics' in test_response.context)
         self.assertTemplateUsed(test_response, 'mysite/actions/topics/topic_edit.html')
+        self.assertEqual(test_response.context['topics'][0].topic, 'Topic1')
+        self.assertEqual(test_response.context['topics'][0].id, 5)
         self.assertEqual(test_response.context['topics'][1].topic, 'Topic2')
+        self.assertEqual(test_response.context['topics'][1].id, 6)
 
     def test_topic_create_view(self):
-        pass
+        test_response = self.client.get('/topic/create')
+        self.assertEqual(test_response.status_code, 200)
+        self.assertTemplateUsed(test_response, 'mysite/actions/topics/create.html')
 
     def test_topic_update_view(self):
-        pass
+        test_response = self.client.get('/topic/7/update')
+        self.assertEqual(test_response.status_code, 200)
+        self.assertTrue('topic' in test_response.context)
+        self.assertTemplateUsed(test_response, 'mysite/actions/topics/update.html')
+        self.assertEqual(test_response.context['topic'].pk, 7)
+        self.assertEqual(test_response.context['topic'].topic, 'Topic1')
+        null_response = self.client.get('/topic/11/update')
+        self.assertEqual(null_response.status_code, 404)
 
     def test_topic_delete_view(self):
-        pass
+        test_response = self.client.get('/topic/3/delete')
+        self.assertEqual(test_response.status_code, 200)
+        self.assertTrue('topic' in test_response.context)
+        self.assertTemplateUsed(test_response, 'mysite/actions/topics/delete.html')
+        self.assertEqual(test_response.context['topic'].pk, 3)
+        self.assertEqual(test_response.context['topic'].topic, 'Topic1')
+        null_response = self.client.get('/topic/11/delete')
+        self.assertEqual(null_response.status_code, 404)
 
-class ArticleViewTest(TestCase):
-    def setUp(self):
-        pass
-
-    def test_article_list_view(self):
-        # main view
-        pass
-
-    def test_article_view(self):
-        pass
-
-    def test_article_create_view(self):
-        pass
-
-    def test_article_update_view(self):
-        pass
-
-    def test_article_delete_view(self):
-        pass
-
-
-class CommentViewTest(TestCase):
-    def setUp(self):
-        pass
-
-    def test_comment_list_view(self):
-        # article view
-        pass
-
-    def test_comment_create_view(self):
-        # article view
-        pass
-
-    def test_comment_update_view(self):
-        pass
-
-    def test_comment_delete_view(self):
-        pass
+# class ArticleViewTest(TestCase):
+#     def setUp(self):
+#         pass
+#
+#     def test_article_list_view(self):
+#         # main view
+#         pass
+#
+#     def test_article_view(self):
+#         pass
+#
+#     def test_article_create_view(self):
+#         pass
+#
+#     def test_article_update_view(self):
+#         pass
+#
+#     def test_article_delete_view(self):
+#         pass
+#
+#
+# class CommentViewTest(TestCase):
+#     def setUp(self):
+#         pass
+#
+#     def test_comment_list_view(self):
+#         # article view
+#         pass
+#
+#     def test_comment_create_view(self):
+#         # article view
+#         pass
+#
+#     def test_comment_update_view(self):
+#         pass
+#
+#     def test_comment_delete_view(self):
+#         pass
